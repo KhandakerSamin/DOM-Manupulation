1f763b15edb331b3521b4c4e89deaa97f9da5df2
Khandaker Samin Yeasar <khandakersaminyeasar@gmail.com>
2023-08-18 16:35:54 +0600

    style-done

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('show-all-btn').addEventListener('click',function(){
+    const classChange = document.getElementById('show-all-class');
+    classChange.removeAttribute('class','hidden'); 
+})
